ac8c7808d70c6b032f3eb873b91b2f24994b681d
kangminhyuk1111 <rkdglqkr12@naver.com>
2022-07-10 17:50:06 +0900

    20220710

--- src/pages/SideLinks.js
+++ src/pages/SideLinks.js
@@ -2,10 +2,28 @@ import React, { useState } from 'react';
 import '../css/sideLinks.css';
 
 export default function SideLinks() {
-    const [burgerMenu , setburgerMenu] = useState();
+    const [burgerMenu , setburgerMenu] = useState(()=>false);
+    const burgerState = () =>{
+      if(burgerMenu == false){
+        setburgerMenu(true)
+      }else{
+        setburgerMenu(false)
+      }
+    }
   return (
         <div className='sideLinks'>
-            <a onClick={"0"}><i class="fa-solid fa-bars"></i></a>
+            <a
+              onClick={burgerState}
+              className={burgerMenu ? "burger_menu" : "burger_menu_hide"}><i className="fa-solid fa-bars"></i></a>
+            <div className='burgerLinks'>
+              <ul className='burger_ul'>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+              </ul>
+            </div>
         </div>
   )
 }
